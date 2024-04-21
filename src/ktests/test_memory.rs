174ab6c12652ca1bc72a1205ecb356d96ee7431b
kai_Ker <kai_Ker@buaa.edu.cn>
2024-04-21 23:57:42 +0800

    [Test] Simple test for the pages

--- src/ktests/test_memory.rs
+++ src/ktests/test_memory.rs
@@ -2,18 +2,63 @@
 pub fn test_page(
     page_free_list: &mut crate::kern::pmap::PageList,
     pages: &mut *mut crate::kern::pmap::PageNode,
-    freemem: &mut usize,
-    npage: usize,
+    _freemem: &mut usize,
+    _npage: usize,
 ) {
-    let pp0 = crate::kern::pmap::page_alloc(page_free_list, pages).unwrap();
-    let pp1 = crate::kern::pmap::page_alloc(page_free_list, pages).unwrap();
-    let pp2 = crate::kern::pmap::page_alloc(page_free_list, pages).unwrap();
+    use crate::kdef::mmu::{PTE_C_CACHEABLE, PTE_V};
+    use crate::kern::pmap::{page_alloc, page_free, page_insert, PageList, PageNode};
+    use crate::{kern::pmap::Pde, page2kva};
+
+    let pp = page_alloc(page_free_list, pages).unwrap();
+    let boot_pgdir = page2kva!(pp, *pages; PageNode) as *mut Pde;
+
+    let mut pp0 = page_alloc(page_free_list, pages).unwrap();
+    let pp1 = page_alloc(page_free_list, pages).unwrap();
+    let mut pp2 = page_alloc(page_free_list, pages).unwrap();
     assert!(!pp0.is_null());
     assert!(!pp1.is_null() && pp1 != pp0);
     assert!(!pp2.is_null() && pp2 != pp0);
 
-    let mut zfl = crate::kern::pmap::PageList::new();
-    assert!(crate::kern::pmap::page_alloc(&mut zfl, pages).is_err());
+    crate::println!(
+        "Normal page_alloc test over. boot_pgdir = 0x{:x}",
+        boot_pgdir as usize
+    );
+
+    let mut zfl = PageList::new();
+    assert!(page_alloc(&mut zfl, pages).is_err());
+    assert!(unsafe { page_insert(boot_pgdir, 0, 0, 0, pp1, &mut zfl, pages).is_err() });
+    page_free(&mut zfl, &mut pp0);
+    assert!(unsafe { page_insert(boot_pgdir, 0, 0, 0, pp1, &mut zfl, pages).is_ok() });
+
+    assert_eq!(PTE_C_CACHEABLE | PTE_V, unsafe {
+        core::ptr::read(boot_pgdir) & 0xFFF
+    });
+
+    assert_eq!(crate::page2pa!(pp0, *pages; PageNode) as u32, unsafe {
+        core::ptr::read(boot_pgdir) & !0xFFF
+    });
+
+    assert_eq!(PTE_C_CACHEABLE | PTE_V, unsafe {
+        core::ptr::read(crate::page2kva!(pp0, *pages; PageNode) as *mut u32) & 0xFFF
+    });
+
+    crate::println!("Empty page_free_list test over.");
+
+    let temp = crate::page2kva!(pp2, *pages; PageNode) as *mut u32;
+    unsafe {
+        *temp = 1000;
+    }
+    page_free(page_free_list, &mut pp2);
+    assert_eq!(1000, unsafe { *temp });
+
+    pp0 = page_alloc(page_free_list, pages).unwrap();
+    assert!(!pp0.is_null());
+
+    let temp2 = crate::page2kva!(pp2, *pages; PageNode) as *mut u32;
+    assert_eq!(temp, temp2);
+    assert_eq!(0, unsafe { *temp2 });
+
+    crate::println!("Directely assignment test over.");
 }
 
 #[cfg(ktest_item = "memory")]
