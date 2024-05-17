d98900b65d72d6407bd18cba84323d6c0b93cfd3
kai_Ker <kai_Ker@buaa.edu.cn>
2024-05-17 13:17:33 +0800

    [Fix]  Switch to Cargo instruction compatible with older versions of Cargo (for geiger exactly)

--- build.rs
+++ build.rs
@@ -8,8 +8,8 @@ const TEST_MOS_RUN_ENV: &str = "run_env";
 const CFG_TEST_ITEM: &str = "ktest_item";
 
 fn main() {
-    println!("cargo::rerun-if-env-changed={}", ENV_MOS_TEST);
-    println!("cargo::rerun-if-env-changed={}", ENV_MOS_RUN_ENV);
+    println!("cargo:rerun-if-env-changed={}", ENV_MOS_TEST);
+    println!("cargo:rerun-if-env-changed={}", ENV_MOS_RUN_ENV);
 
     match env::var_os(ENV_MOS_TEST) {
         None => (),
@@ -18,7 +18,7 @@ fn main() {
             if test == TEST_MOS_RUN_ENV {
                 handle_run_env();
             }
-            println!("cargo::rustc-cfg={}=\"{}\"", CFG_TEST_ITEM, test);
+            println!("cargo:rustc-cfg={}=\"{}\"", CFG_TEST_ITEM, test);
         }
     }
 }
