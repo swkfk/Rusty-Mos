263893c7cc059155b9894ae154beb823b95a70e2
kai_Ker <kai_Ker@buaa.edu.cn>
2024-06-24 10:12:51 +0800

    [Fix]  The `do_syscall` will retuen -E_NO_SYS if the sysno is invalid

--- src/arch_mipsel/syscall_impl.rs
+++ src/arch_mipsel/syscall_impl.rs
@@ -391,7 +391,7 @@ pub fn do_syscall(trapframe: *mut TrapFrame) {
     let tf = unsafe { *tf_p };
     let sysno = tf.regs[4];
     if !(0..MAX_SYS_NO as u32).contains(&sysno) {
-        unsafe { (*tf_p).regs[2] = KError::NoSys as u32 }
+        unsafe { (*tf_p).regs[2] = KError::NoSys.into() }
         return;
     }
     unsafe { (*tf_p).cp0_epc += size_of::<u32>() as u32 }
